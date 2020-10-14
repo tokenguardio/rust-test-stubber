c16ed216c9c9d6c118c49a1137303c47317e4dad
Konrad Kleczkowski <konrad@JAPER-PC.localdomain>
2020-10-14 15:02:07 +0200

    I/O moved to the main module

--- src/ast.rs
+++ src/ast.rs
@@ -1,92 +1,17 @@
-use std::{fs, io, io::Read, io::Write};
+/// This module contains functions that allows to create test stubs.
+use quote::quote;
 use syn::export::Span;
-extern crate quote;
-use quote::*;
+use syn::Ident;
 
-#[derive(Debug)]
-pub enum Error {
-    IOError(io::Error),
-    ParseError(syn::Error)
+pub fn process_string(content: String) -> Result<String, syn::Error> {
+    let mut file = syn::parse_file(&content)?;
+    file.items.push(syn::Item::Mod(traverse_items(&file.items)));
+    Ok(format!("{}", quote!(#file)))
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
-
-pub fn process_file(in_path: &str, out_path: &str) -> Result<()> {
-    let mut file = load_file(in_path)?;
-    prepend_test_mod(&mut file);
-    let mut out = fs::File::create(out_path).map_err(|e| Error::IOError(e))?;
-    out.write_fmt(format_args!("{}", quote::quote!(#file))).map_err(|e| Error::IOError(e))?;
-    Ok(())
-}
-
-pub fn process_string(contents: String) -> Result<String> {
-    let mut file = syn::parse_file(&contents).map_err(|e| Error::ParseError(e))?;
-    prepend_test_mod(&mut file);
-    Ok(format!("{}", quote::quote!(#file)))
-}
-
-/// Loads file in a Result.
-fn load_file(path: &str) -> Result<syn::File> {
-    fn get_contents(path: &str) -> io::Result<String> {
-        let mut file = fs::File::open(path)?;
-        let mut buf = String::new();
-        file.read_to_string(&mut buf)?;
-        Ok(buf)
-    }
-    let contents = get_contents(path).map_err(|e| Error::IOError(e))?;
-    let ast = syn::parse_file(&contents).map_err(|e| Error::ParseError(e))?;
-    Ok(ast)
-}
-
-fn gen_method_stub(name: &str) -> syn::ItemFn {
-    let fmt_name = format!("test_{}", name);
-    let id = syn::Ident::new(&fmt_name, Span::call_site());
-    syn::parse_quote! {
-        #[test]
-        fn #id() {
-            // TODO
-        }
-    }
-}
-
-fn gen_trait_stub(item_trait: syn::ItemTrait) -> (syn::ItemStruct, syn::ItemImpl) {
-    let name = item_trait.ident;
-    let fake_struct_name = format!("Fake{}", name.to_string());
-    let fake_struct_id = syn::Ident::new(&fake_struct_name, Span::call_site());
-
-    let stubs: Vec<syn::ImplItemMethod> = item_trait
-        .items
-        .into_iter()
-        .filter_map(|it| match it {
-            syn::TraitItem::Method(method) => 
-                Some(syn::ImplItemMethod {
-                    attrs: method.attrs, 
-                    vis: syn::Visibility::Inherited,
-                    defaultness: None,
-                    sig: method.sig,
-                    block: syn::parse_quote! {
-                        {
-
-                        }
-                    }
-                }),
-            _ => None,
-        })
-        .collect();
-    
-    let ast_struct: syn::ItemStruct = syn::parse_quote! {
-        struct #fake_struct_id;
-    };
-    let ast_impl: syn::ItemImpl = syn::parse_quote! {
-        impl #name for #fake_struct_id {
-            #(#stubs)*
-        }
-    };
-
-    (ast_struct, ast_impl)
-}
-
-fn traverse_items(items: Vec<syn::Item>) -> syn::ItemMod {
+/// Traverses file item by item, generating test module that contains method stubs
+/// and mocks.
+pub fn traverse_items(items: &Vec<syn::Item>) -> syn::ItemMod {
     let (structs, impls): (Vec<syn::ItemStruct>, Vec<syn::ItemImpl>) = items
         .clone()
         .into_iter()
@@ -143,6 +68,50 @@ fn traverse_items(items: Vec<syn::Item>) -> syn::ItemMod {
     }
 }
 
-fn prepend_test_mod(file: &mut syn::File) {
-    file.items.push(syn::Item::Mod(traverse_items(file.items.clone())));
+fn gen_method_stub(name: &str) -> syn::ItemFn {
+    let fmt_name = format!("test_{}", name);
+    let id = syn::Ident::new(&fmt_name, Span::call_site());
+    syn::parse_quote! {
+        #[test]
+        fn #id() {
+            // TODO
+        }
+    }
+}
+
+fn gen_trait_stub(item_trait: syn::ItemTrait) -> (syn::ItemStruct, syn::ItemImpl) {
+    let fake_struct_name = format!("Fake{}", item_trait.ident.to_string());
+    let fake_struct_id = Ident::new(&fake_struct_name, Span::call_site());
+
+    let stubs: Vec<syn::ImplItemMethod> = item_trait
+        .items
+        .into_iter()
+        .filter_map(|it| match it {
+            syn::TraitItem::Method(method) => 
+                Some(syn::ImplItemMethod {
+                    attrs: method.attrs, 
+                    vis: syn::Visibility::Inherited,
+                    defaultness: None,
+                    sig: method.sig,
+                    block: syn::parse_quote! {
+                        {
+
+                        }
+                    }
+                }),
+            _ => None,
+        })
+        .collect();
+    
+    let ast_struct: syn::ItemStruct = syn::parse_quote! {
+        struct #fake_struct_id;
+    };
+    
+    let ast_impl: syn::ItemImpl = syn::parse_quote! {
+        impl #(item_trait.ident) for #fake_struct_id {
+            #(#stubs)*
+        }
+    };
+
+    (ast_struct, ast_impl)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate clap;
+use std::fs;
 use clap::App;
 
 mod ast;
@@ -17,7 +18,17 @@ fn main() {
             .value_of("OUTPUT")
             .expect("Argument is mandatory");
 
-    ast::process_file(input, output)
-        .expect("Cannot process the file");
+    process_file(input, output);
+}
+
+fn process_file(in_path: &str, out_path: &str) {
+    let input 
+        = fs::read_to_string(in_path)
+            .expect("Couldn't open the input file");
+    let processed
+        = ast::process_string(input)
+            .expect("Couldn't parse input file");
+    fs::write(out_path, processed)
+        .expect("Couldn't write to the output file");
 }
     
